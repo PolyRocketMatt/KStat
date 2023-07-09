0858939b67e28b55c12abde59790b0c5e0a6dc49
Matthias Kovacic <rocketpencil.studios@gmail.com>
2023-07-09 21:47:57 +0200

    Update documentation generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+
 plugins {
     kotlin("jvm") version "1.8.0"
     id("org.jetbrains.dokka") version "1.8.20"
@@ -14,12 +17,25 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-base:1.8.20")
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
 
 tasks.dokkaHtml {
     outputDirectory.set(file("docs"))
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
+    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+        customAssets = listOf(file("img/logo-icon.svg"))
+        footerMessage = "© 2023 Matthias Kovacic"
+    }
 }
 
 kotlin {
